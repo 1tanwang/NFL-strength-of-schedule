a8762fbbd9f901fe1cf0ca0ebcaaed5e88ac3779
1tanwang <ytwang.ethan@gmail.com>
2020-02-18 09:07:05 +1000

    [Update] support functionality to expand the csv file with new data

--- nfl_strength_of_schedule.py
+++ nfl_strength_of_schedule.py
@@ -11,6 +11,9 @@ from standing import scrape_standing
 
 
 def main():
+    year = "2020"
+    sos = "sos_" + year
+
     print("----- initilizing webdriver -----")
     options = Options()
     options.add_argument("--headless")
@@ -18,11 +21,12 @@ def main():
 
     try:
         print("----- scraping opponents data -----")
-        opponents = scrape_opponents(driver)
+        opponents = scrape_opponents(driver, year)
         print("----- scraping standing data -----")
-        standing = scrape_standing(driver)
+        standing = scrape_standing(driver, year)
     finally:
         driver.close()
+        driver.quit()
 
     print("----- calculating strength of schedule -----")
 
@@ -30,13 +34,26 @@ def main():
         wins_sum = 0
         for r in row[1:]:
             wins_sum += int(standing.loc[standing["team_names"] == r, "wins"].values[0])
-        standing.loc[standing["team_names"] == row[0], "sos"] = wins_sum/256
+        standing.loc[standing["team_names"] == row[0], sos] = wins_sum/256
+
+    df_sos = standing.loc[:, ["city_names", "team_names", sos]].sort_values(sos, ignore_index=True)
+    df_sos["Team"] = df_sos["city_names"] + " " + df_sos["team_names"]
+    df_sos["rank_"+year] = df_sos.index + 1
+    df_sos[sos] = df_sos[sos].round(3)
+    df_sos = df_sos[["Team", sos, "rank_"+year]]
 
     print("----- 2020 NFL strength of schedule list -----")
-    print(standing.loc[:, ["city_names", "team_names", "sos"]].sort_values("sos", ignore_index=True))
+    print(df_sos)
 
-    file_name = "nfl_strength_of_schedule_2020.csv"
-    standing.loc[:, ["city_names", "team_names", "sos"]].sort_values("sos", ignore_index=True).to_csv(file_name, index=False)
+    file_name = "nfl_strength_of_schedule.csv"
+    try:
+        df_prev = pd.read_csv(file_name)
+        df_sos = df_sos.merge(df_prev, left_on="Team", right_on="Team")
+        df_sos.to_csv(file_name, index=False, float_format="%.3f")
+    except pd.errors.EmptyDataError:
+        df_sos.to_csv(file_name, index=False, float_format="%.3f")
+    except FileNotFoundError:
+        df_sos.to_csv(file_name, index=False, float_format="%.3f")
 
 if __name__ == '__main__':
     main()
